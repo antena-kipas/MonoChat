1bc6b749462bbbb0f63d5be12183536da882d794
antena-kipas <ujankcodet0@gmail.com>
2025-07-19 18:08:20 +0700

    19 juli

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div><h1>Home Page</h1></div>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[15%]'>
+
+    </div>
   )
 }
 
