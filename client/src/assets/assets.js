d809a881e15ef3fc0cf164c0c303a2aae7869b2c
antena-kipas <ujankcodet0@gmail.com>
2025-07-22 15:34:53 +0700

    add some assets to src/assets but there is some error at HomePage.jsx . Next I will fix it

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,11 +1,16 @@
 import bgimage from './bgimage.svg'
+import menu_icon from './menu_icon.png'
+import logo from './logo.png'
 import pic1 from './pic1.png'
 import pic2 from './pic2.png'
-import pic3 from './pic3.png'
+import pic3 from '.pic3.png'
 import pic4 from './pic4.png'
 
 const assets = {
-        bgimage
+        bgimage,
+        logo,
+        menu_icon
+
 }
 
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
+
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import Sidebar from '../components/Sidebar'
 
 const HomePage = () => {
     const [selectedUser, setSelectedUser] = useState(false)
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[15%]'>
-      <div className={`backdrop-blur-sm border-2 border-green-600 overflow-hidden h-[100%] grid grid-cols-1
+      <div className={`backdrop-blur-sm bg-black/50 border-2 border-green-600 overflow-hidden h-[100%] grid grid-cols-1
          relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar />
         <ChatContainer />
