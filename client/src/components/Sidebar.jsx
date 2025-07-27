91fcf5500ed13fd277bd9c60a424f6be0c1cdd96
Linux <linux@localhost.localdomain>
2025-07-28 00:39:28 +0700

    28  jul 52:13, add some assets

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
@@ -26,9 +26,25 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
         </div>
       </div>
 
-    <div className='flex flex-col'>
-      {userDummyData}
-    </div>
+      <div className='flex flex-col'>
+        {userDummyData.map((user, index)=>(
+            <div key={index} className={`relative flex items-center gap-2 p-2 pl-4 cursor-pointer mac-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+              <img src={user?.profilePic || assets.avatar_icon} alt=""
+              className='w-[35px] aspect-[1/1'/>
+              <div className='flex flex-col leading-5'>
+                <p>{user.fullName}</p>
+                {
+                  index < 3
+                  ? <span className='text-green-400 text-xs'>Online</span>
+                  : <span className='text-neutral-400 text-xs'>Offline</span>
+                }
+              </div>
+              {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5
+              flex justify center items-center bg-vio;et-500/50'>{index}</p>}
+            </div>
+        ))}
+      </div>
+
 
     </div>
   )
