8e40b976bf638003fe5ad53e0bdaf60db92283ff
antena-kipas <ujankcodet0@gmail.com>
2025-07-29 19:32:10 +0700

    fix some error at HomePage the box is sm:py[5%] not sm:py[15%]

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ChatContainer() {
   return (
     <div>
-        <h1>ChatContainer</h1>
+        <div
     </div>
   )
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,12 +6,12 @@ import Sidebar from '../components/Sidebar'
 const HomePage = () => {
     const [selectedUser, setSelectedUser] = useState(false)
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[15%]'>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
       <div className={`backdrop-blur-sm bg-black/50 border-2 border-green-600 overflow-hidden h-[100%] grid grid-cols-1
-         relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-        <Sidebar />
-        <ChatContainer />
-        <RightSidebar />
+         relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
       </div>
     </div>
   )
