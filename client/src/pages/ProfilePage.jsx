47230a8f09320e18f719a781bde5098d572f110d
antena-kipas <ujankcodet0@gmail.com>
2025-08-04 21:32:58 +0700

    front end complete next backend

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import assets from '../assets/assets';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -8,22 +9,40 @@ const ProfilePage = () => {
   const [name, setName] = useState("Martin Johnson")
   const [bio, setBio] = useState("Hi Everyone, I am Using Quickchat")
 
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    navigate('/')
+  }
+
 
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center 
     justify-center '>
-      <div className='w-5/6 max-w-2x1 backdrop-blur-2x1 text-gray-300 border-2
-      border-gray-600 flex items-center justify-between max-sm:flex-col-reverse
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2
+      border-green-600 flex items-center justify-between max-sm:flex-col-reverse
       rounded-lg'>
-        <form className='flex flex-col gap-5 p-10 flex-1'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
             <input onChange={(e)=>setSelectedImg(e.target.files[0])} 
-            type="file" id='avatar' accept='.png .jpg .jpeg' hidden/>
-            <img src={""} alt="" />
+            type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.
+              avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`} />
+            upload profile image 
           </label>
+          <input onChange={(e)=>setName(e.target.value)} value={name}
+          type="text" required placeholder='Your Name' className='p-2 border border-
+          green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500' />
+            <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+            placeholder='Write profil bio' required className='p-2 border
+            border-green-500 rounded-md focus:outline-none focus:ring-2 focus:
+            ring-green-400' rows={4}></textarea>
+            <button type="submit" className='bg-gradient-to-r from-green-300
+            to-green-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save
+            </button>
         </form>
-        <img src="" alt="" />
+        <img className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt10" 
+        src={assets.logo_icon} alt="" />
       </div>
 
     </div>
